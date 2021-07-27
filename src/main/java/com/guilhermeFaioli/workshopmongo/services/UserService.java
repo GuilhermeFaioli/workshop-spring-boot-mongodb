4a53fbd4f425b078099a927a615851a08216ff8b
GuilhermeFaioli <guilhermefa12@gmail.com>
2021-07-27 11:06:38 -0300

    Obtendo um usuario por id

--- src/main/java/com/guilhermeFaioli/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermeFaioli/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.guilhermeFaioli.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermeFaioli.workshopmongo.domain.User;
 import com.guilhermeFaioli.workshopmongo.repository.UserRepository;
+import com.guilhermeFaioli.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }

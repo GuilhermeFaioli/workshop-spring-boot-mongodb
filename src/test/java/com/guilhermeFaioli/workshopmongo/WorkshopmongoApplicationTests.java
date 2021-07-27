229af64524914784fb555600eab43b014e5a9d89
GuilhermeFaioli <guilhermefa12@gmail.com>
2021-07-27 09:18:44 -0300

    first commit

---
+++ src/test/java/com/guilhermeFaioli/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeFaioli.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
